827cd77e6b5d7f6e9445a129405f9e740ca1d37a
nobody <donotbotherme@vacation.com>
2018-11-27 21:00:00 +0000

    ;;

--- alis.sh
+++ alis.sh
@@ -2,6 +2,7 @@
 
 args=("$@")
 
+echo "Setting default"
 
 installDir='/mnt'
 rootPart=''
@@ -9,60 +10,62 @@ homePart=''
 bootPart=''
 kbLayout=''
 
+echo "Parsing args"
 for (( i=0 ; i<$# ; i++ ))
 do
 	case "${args[i]}" in
 		"-i" | "--install")
 			((i++))
-			$installDir=${args[i]}
+			installDir=${args[i]}
 			;;
 		"-r" | "--root")
 			((i++))
-			$rootPart=${args[i]}
+			rootPart=${args[i]}
 			;;
 		"-h" | "--home")
 			((i++))
-			$homePart=${args[i]}
+			homePart=${args[i]}
 			;;
 		"-b" | "--boot")
 			((i++))
-			$bootPart=${args[i]}
+			bootPart=${args[i]}
 			;;
 		"-k" | "--kblayout")
 			((i++))
-			$kbLayout=${args[i]}
+			kbLayout=${args[i]}
 			;;
 		
 	esac
-
-	if [ -z "$rootPart" ]; then exit(1); fi
-	if [ -z "$homePart" ]; then $homePart=$rootPart; fi
-	if [ -z "$bootPart" ]; then $bootPart=$rootPart; fi
-
-	#install base package
-
-	echo "mounting root $rootPart on $installDir"
-	mount $rootPart $installDir
-
-	if [ -z "@homePart" ];
-	then
-		echo "mounting home $home on $installDir/home"
-		mkdir $installDir/home
-		mount $homePart $installDir/home
-	fi
-
-
-	if [ -z "@bootPart" ];
-	then
-		echo "mounting boot $boot on $installDir/boot"
-		mkdir $installDir/boot
-		mount $bootPart $installDir/boot
-	fi
-		
-
-	echo "Installing packages base and base-devel to $installDir"
-	pacstrap ${installDir} base base-devel && genfstab $installDir >> $installDir/etc/fstab
-
-	echo "yay"
-
 done
+
+
+echo "Checking if Partitions differ from default"
+
+if [ -z $rootPart ]; then exit 1; fi
+
+#install base package
+
+echo "mounting root $rootPart on $installDir"
+mount $rootPart $installDir
+
+if [ -n $homePart ];
+then
+	echo "mounting home $home on $installDir/home"
+	mkdir $installDir/home
+	mount $homePart $installDir/home
+fi
+
+
+if [ -n $bootPart ];
+then
+	echo "mounting boot $boot on $installDir/boot"
+	mkdir $installDir/boot
+	mount $bootPart $installDir/boot
+fi
+	
+
+echo "Installing packages base and base-devel to $installDir"
+pacstrap ${installDir} base base-devel && genfstab $installDir >> $installDir/etc/fstab
+
+echo "yay"
+
